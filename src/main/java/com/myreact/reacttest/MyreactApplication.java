10c230554bc19300676edad35c1139da223d13a7
freegastello <freegastello@gmail.com>
2025-02-22 17:13:52 +0300

    250222= Первый коммит

---
+++ src/main/java/com/myreact/reacttest/MyreactApplication.java
@@ -0,0 +1,13 @@
+package com.myreact.reacttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyreactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyreactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myreact/reacttest/MyreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreact.reacttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyreactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
